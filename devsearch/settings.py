79632592d4eb08965b665ff9a025b17f2291f2ad
Md Tanvir Reza <59385180+tanvir-reza@users.noreply.github.com>
2023-02-02 05:58:07 +0600

    rail final

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r%*djq*%ywrc_0v=+5@ue!e&0a73hk949s7hyn4a59^!6ad!h='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = TRUE
+DEBUG = False
 
 
 ALLOWED_HOSTS = ['*']
