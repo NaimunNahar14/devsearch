14e242bc2e6d0f57954836e1a062999561bd5fbc
IsmatJahanBijori <121631448+IsmatJahanBijori@users.noreply.github.com>
2023-03-09 12:38:59 +0600

    bijori
    url update

--- users/urls.py
+++ users/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
 
     path('login',loginPage,name='login'),
     path('logout',logoutUser,name='logout'),
-    path('signup',signupUser,name='signup'),
+    path('signup',signupUser,name='signupuser'),
     path('user',userAccount,name='user'),
     path('edit-profile',editProfile,name='editprofile'),
     path('',profiles,name='profiles'),
